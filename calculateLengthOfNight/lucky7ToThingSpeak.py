52bab3ddb0d96b6069c2c4a040efc13ce552389d
Alfred Lorber <aalorbe@sandia.gov>
2016-10-30 21:23:57 -0600

    lucky7ToThingSpeak.py: First working, robust version.  Latest versions of other files

--- calculateLengthOfNight/lucky7ToThingSpeak.py
+++ calculateLengthOfNight/lucky7ToThingSpeak.py
@@ -4,7 +4,16 @@ import serial
 import os
 import sys
 import time
+import ast
+import thingspeak
 
+# ThingSpeak keys
+channel_id = "176582"
+write_key  = "VHTF9TVYRZCCPY77"
+
+frequency = 60 # Record data at this frequency in seconds
+
+modeMap = {'O':0,'B':1,'E':2,'N':3,'P':4,'M':5,'D':6}
 
 def createDateStamp():
   return time.strftime("%Y-%m-%d",time.localtime())
@@ -17,13 +26,15 @@ def makeOutputFileName(outputFileRoot, dateStamp):
 
 def main(cmdLineArgs = sys.argv):
 
+  channel = thingspeak.Channel(id=channel_id,write_key=write_key)
+
   currentDateStamp = None
   outputStream     = None
 
   if len(cmdLineArgs) != 2:
     print >>sys.stderr, "readArdiono: Need two args: <serial_port> <output_file_root>"
-    print >>sys.stderr, "             Example: readArdiono /dev/cu.usbmodemfd121 readArduino"
-    print >>sys.stderr, "             Produces the log file readArduino.2015-08-17.log on Aug. 17, 2015"
+    print >>sys.stderr, "             Example: readArdiono /dev/cu.usbmodemfd121 lucky7ToThingSpeak"
+    print >>sys.stderr, "             Produces the log file lucky7ToThingSpeak.2015-08-17.log on Aug. 17, 2015"
     sys.exit(1)
 
   serialPort = cmdLineArgs[0]
@@ -33,14 +44,18 @@ def main(cmdLineArgs = sys.argv):
   print "outputFileRoot =", outputFileRoot
 
   ser = serial.Serial(serialPort,115200)
+  time.sleep(5)
+  localFrequency = 5
 
   while True:
+    ser.write('?')
     buffer = ser.read(ser.inWaiting())
     lines = buffer.split('\r\n')
     for line in lines:
       if line:
         print '%s' % line.strip()
-        if line[0:7] == "millis,":
+        if line[0] == "{":
+          localFrequency = frequency
           if currentDateStamp != createDateStamp():
             currentDateStamp = createDateStamp()
             outputFileName = makeOutputFileName(outputFileRoot, createDateStamp())
@@ -48,11 +63,29 @@ def main(cmdLineArgs = sys.argv):
             if outputStream:
               outputStream.close()
             outputStream = makeOutputStream(outputFileName)
-          msg = "%s, %s" % (time.strftime("%Y-%m-%d, %H:%M:%S",time.localtime()), line)
-          print msg
-          print >>outputStream , msg
+          outputDict = ast.literal_eval(line)
+          print outputDict
+          print >>outputStream , outputDict
           outputStream.flush()
-    time.sleep(1)
+          channelKeys = [1,2,5,6]
+          channelDict = {1:outputDict[channelKeys[0]][0],
+                         2:outputDict[channelKeys[1]][0],
+                         3:outputDict[channelKeys[2]][0],
+                         4:outputDict[channelKeys[3]][0],
+                         5:outputDict['v'],
+                         6:outputDict['p'],
+                         7:modeMap[outputDict['m']],
+                         8:outputDict['lN']}
+          print "channelDict =", channelDict
+          try:
+            response = channel.update(channelDict)
+            print response
+          except Exception, e:
+            print "channel.update(channelDict) failed:"
+            print str(e)
+
+    print "Sleeping for %s seconds" % localFrequency
+    time.sleep(localFrequency)
 
   outputStream.close()
 
